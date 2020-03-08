0975c6d96d77d78895de6e9f59c0e4e15f744b2e
PythonFZ <iXvZgs3LBAFROWBa>
2020-03-08 17:27:56 +0100

    Change Timezone, Remove "!"

--- ChemData/settings.py
+++ ChemData/settings.py
@@ -111,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
